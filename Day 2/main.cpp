d714ebf38eac0c1edfab83b7f4386454564c86c4
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-12-02 17:39:23 +0100

    Day 2 part 1 skeleton

---
+++ Day 2/main.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+
+void read_file(const char[] &filename) {
+    
+}
+
+
+
+int how_many_safe(std::ifstream file) {
+    for (file) {
+        is_increasing();
+        is_decreasing();
+    }
+
+}
+
+
+int main(int argc, char *argv[]) {
+    input = read_file(argv[1]);
+    how_many_safe(input);
+}
