87d134fc88646357bbb73484c437810f933e7be2
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-12-02 20:18:26 +0100

    Classes

--- Day 2/main.cpp
+++ Day 2/main.cpp
@@ -29,49 +29,53 @@ std::vector<std::vector<int>> read_file(const std::string& filename) {
     return reports;
 }
 
-bool is_increasing(const std::vector<int>& nums) {
-    for (size_t i = 1; i < nums.size(); i++) {
-        if (nums[i] <= nums[i-1]) {
-            return false;
+class Part1 {
+    bool is_increasing(const std::vector<int>& nums) {
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i-1]) {
+                return false;
+            }
         }
-    }
     
-    return true; 
-}
-
-bool is_decreasing(const std::vector<int>& nums) {
-    for (size_t i = 1; i < nums.size(); i++) {
-        if (nums[i] >= nums[i-1]) {
-
-            return false;
-        }
+        return true; 
     }
 
-    return true;
-}
-
-bool correct_steps(const std::vector<int>& nums) {
-    // correct steps: 1, 2, 3
-    for (size_t i = 1; i < nums.size(); i++) {
-        if (std::abs(nums[i] - nums[i-1]) > 3) {
-            return false;
+    bool is_decreasing(const std::vector<int>& nums) {
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (nums[i] >= nums[i-1]) {
+                return false;
+            }
         }
+
+        return true;
     }
+
+    bool correct_steps(const std::vector<int>& nums) {
+        // correct steps: 1, 2, 3
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (std::abs(nums[i] - nums[i-1]) > 3) {
+                return false;
+            }
+        }
     
-    return true;
-}
-
-
-void how_many_safe(std::vector<std::vector<int>>& input) {
-    int safe_count = 0;
-    for (std::vector<int> report : input) {
-        if (is_increasing(report) && correct_steps(report) || is_decreasing(report) && correct_steps(report)) {
-            safe_count++;
-        }
+        return true;
     }
-    std::cout << safe_count << std::endl;
-}
 
+
+    void how_many_safe(std::vector<std::vector<int>>& input) {
+        int safe_count = 0;
+        for (std::vector<int> report : input) {
+            if (is_increasing(report) && correct_steps(report) || is_decreasing(report) && correct_steps(report)) {
+                safe_count++;
+            }
+        }
+        std::cout << safe_count << std::endl;
+    }
+};
+
+class Part2 {
+    
+};
 
 int main(int argc, char *argv[]) {
     std::vector<std::vector<int>> input = read_file(argv[1]);
